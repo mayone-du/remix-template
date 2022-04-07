9ca5990b3b30dce3e64e6530ce7daa63564f9899
mayone-du <cocomayo1201@icloud.com>
2022-04-07 23:11:03 +0900

    init: create-remix@latest with cloudflare workers

---
+++ app/root.tsx
@@ -0,0 +1,39 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/cloudflare";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import globalStylesUrl from "~/styles/global.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: globalStylesUrl },
+];
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+const App = () => {
+  return (
+    <html lang="ja">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+};
+
+export default App;
---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Welcom Remix Template!</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
